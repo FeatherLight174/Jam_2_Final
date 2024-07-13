fcf002bb771fded7fb6687e29fdf61db5ea00e10
FeatherLight174 <songyh2022@shanghaitech.edu.cn>
2024-07-13 20:48:20 +0800

    pause audio

--- Assets/Script/InvisibleObject.cs
+++ Assets/Script/InvisibleObject.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class InvisibleObject : MonoBehaviour
 {
     public GameObject invisibleObstacle;
+    public MissTime missTime;
     // Start is called before the first frame update
     void Start()
     {
